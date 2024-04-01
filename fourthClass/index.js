13f4077853c2ab6495526af299a99e81e0f82c90
Md. Shadikur Rahman Sheam <140042996+sadikurSenpai@users.noreply.github.com>
2024-04-01 12:41:07 +0600

    Add files via upload

---
+++ fourthClass/index.js
@@ -0,0 +1,36 @@
+// let firstName = 'Sadikur'
+// console.log(firstName);
+
+// function add_two_numbers(n1, n2){
+//     return n1+n2;
+// }
+// this.document.getElementById('heading').innerHTML = add_two_numbers(10, 20);
+
+// let person = {
+//     name : 'Sadikur',
+//     age : 23,
+//     id : 211902020
+// };
+
+// console.log(person)
+// person.name = 'Shadikur'
+// console.log(person)
+// person['age'] = 22
+// console.log(person)
+// const ID = 'id';
+// person[ID] = 20
+// console.log(person)
+// let id = [50, 20, 30, 40];
+// console.log(id)
+// console.log(id[0])
+// console.log(id[5])
+// id.sort()
+// console.log(id)
+// console.log(id.length)
+
+function greet(name){
+    console.log("Hello "+name)
+}
+
+greet('Sadik')
+greet('Sadikur')
